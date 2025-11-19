7c35fdd1e519128be463d74a79923c90066506ce
李宝旭 <libx@inke.cn>
2025-11-19 18:34:33 +0800

    add a.go

---
+++ src/a.go
@@ -0,0 +1,21 @@
+package src
+
+import "fmt"
+
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func getConfig() (*Response, error) {
+	return nil, nil
+}
+
+func main() {
+	var resp, err = getConfig()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println(resp.Code)
+}
